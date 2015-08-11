d0b5acd29db6fb614f2db94f3da4b312d59e5fd7
Amos Elmaliah <amosel@gmail.com>
2015-08-11 11:42:35 +0300

    Added schematic layout code. Added Schematic Layout connector view kind.

--- CollectionView/SchematicLayoutAttributes.swift
+++ CollectionView/SchematicLayoutAttributes.swift
@@ -1,2 +1,19 @@
 import UIKit
-class SchematicLayoutAttributes: UICollectionViewLayoutAttributes {}
+
+class SchematicLayoutAttributes: UICollectionViewLayoutAttributes {
+    var connectorLineStartTop: Bool = true
+    
+    override func copyWithZone(zone: NSZone) -> AnyObject {
+        var copy = super.copyWithZone(zone) as! SchematicLayoutAttributes
+        copy.connectorLineStartTop = self.connectorLineStartTop
+        return copy
+    }
+}
+
+class SchematicLayout {
+    static var connectorViewKind:String {
+        get {
+            return "SchematicConnector"
+        }
+    }
+}
