02ae06101ac73a31de4ee12ca874b3c8af5050ce
Amos Elmaliah <amosel@gmail.com>
2015-08-16 15:56:16 +0300

    Collection type now responds to GroupBy and Array responds to count(test)

--- CollectionView/Extension.swift
+++ CollectionView/Extension.swift
@@ -16,6 +16,9 @@ extension Array {
         }
         return false
     }
+    func count(test:(Element)->Bool ) -> Int {
+        return reduce(0) { if test($1) { return $0 + 1 } else { return $0 } }
+    }
 }
 
 
@@ -27,3 +30,35 @@ extension Dictionary {
         return nil
     }
 }
+
+extension CollectionType {
+    typealias Element = Generator.Element
+    
+    func groupBy<Key : Hashable >(fn:(Element)->(Key)) -> [Key:[Element]] {
+        typealias Bundle = [Key:[Element]]
+        return reduce(Bundle()) { (var bundle, element) in
+            let key = fn(element)
+            if bundle[key] != nil {
+                bundle[key]!.append(element)
+            } else {
+                bundle[key] = [element]
+            }
+            return bundle
+        }
+        
+    }
+    
+    func groupBy<Key : Hashable, NewElement >(fn:(Element)->(Key,NewElement)) -> [Key:[NewElement]] {
+        typealias Bundle = [Key:[NewElement]]
+        return reduce(Bundle()) { (var bundle, element) in
+            let (key, newElement) = fn(element)
+            if bundle[key] != nil {
+                bundle[key]!.append(newElement)
+            } else {
+                bundle[key] = [newElement]
+            }
+            return bundle
+        }
+    }
+}
+
